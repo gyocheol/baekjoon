82af7d327659057e212837bf11c8a6752272647b
강교철 <96896754+gyocheol@users.noreply.github.com>
2022-05-23 15:03:11 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,10 @@
+txt = list(input())
+result = []
+ask = 97
+for i in range(26):
+    if chr(ask) in txt:
+        result.append(str(txt.index(chr(ask))))
+    else:
+        result.append(str(-1))
+    ask+=1
+print(' '.join(result))
