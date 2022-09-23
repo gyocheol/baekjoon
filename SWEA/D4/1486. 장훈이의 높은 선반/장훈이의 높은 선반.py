034be9c9ebaa3c1e573e2308162c25c0d9268750
강교철 <96896754+gyocheol@users.noreply.github.com>
2022-09-23 14:38:01 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 1,051 ms, Memory: 74,904 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -0,0 +1,23 @@
+def comb(n, r, k, s):
+    global ans
+    if k == r:
+        if 0 <= sum(t) - B < ans:
+            ans = sum(t) - B
+        return
+    else:
+        for i in range(s, n-r+1+k):
+            t[k] = arr[i]
+            comb(n, r, k+1, i+1)
+
+
+for t in range(int(input())):
+    print(f'#{t+1}', end=' ')
+    N, B = map(int, input().split())
+    arr = list(map(int, input().split()))
+    arr.sort(reverse=True)
+    ans = 10**10
+    for i in range(1, N+1):
+        R = i
+        t = [0] * R
+        comb(N, R, 0, 0)
+    print(ans)
