cd3a9046360e26e5a16538ba1cf10d5a4a96f775
강교철 <96896754+gyocheol@users.noreply.github.com>
2022-09-25 22:07:19 +0900

    [D2] Title: 파리퇴치3, Time: 209 ms, Memory: 61,692 KB -BaekjoonHub

--- SWEA/D2/12712. 파리퇴치3/파리퇴치3.py
+++ SWEA/D2/12712. 파리퇴치3/파리퇴치3.py
@@ -1,48 +1,46 @@
-# 우좌하상
-def solve(a):
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
+def solve1():
     ans = 0
     for i in range(N):
         for j in range(N):
-            total = 0
-            total += a[i][j]
-            for x in range(4):
-                for z in range(1, M):
-                    if N > i+dx[x]*z >= 0 and N > j+dy[x]*z >= 0:
-                        total += a[i+dx[x]*z][j+dy[x]*z]
-                    else:
-                        pass
-            if total > ans:
-                ans = total
-    return ans
-
-# 대각선
-def solve2(a):
-    dx = [1, -1, -1, 1]
-    dy = [-1, -1, 1, 1]
-    ans = 0
-    for i in range(N):
-        for j in range(N):
-            total = 0
-            total += a[i][j]
-            for x in range(4):
-                for z in range(1, M):
-                    if N > i + dx[x] * z >= 0 and N > j + dy[x] * z >= 0:
-                        total += a[i + dx[x] * z][j + dy[x] * z]
-                    else:
-                        pass
+            total = arr[i][j]
+            for k in range(1, M):
+                for z in range(4):
+                    ii = i + di[z] * k
+                    jj = j + dj[z] * k
+                    if 0 <= ii < N and 0 <= jj < N:
+                        total += arr[ii][jj]
             if total > ans:
                 ans = total
     return ans
 
 
-T = int(input())
-for t in range(1, T+1):
+def solve2():
+    ans = 0
+    for x in range(N):
+        for y in range(N):
+            total = arr[x][y]
+            for k in range(1, M):
+                for z in range(4):
+                    xx = x + dx[z] * k
+                    yy = y + dy[z] * k
+                    if 0 <= xx < N and 0 <= yy < N:
+                        total += arr[xx][yy]
+            if total > ans:
+                ans = total
+    return ans
+
+
+di = [1, -1, 0, 0]
+dj = [0, 0, 1, -1]
+dx = [-1, 1, -1, 1]
+dy = [-1, -1, 1, 1]
+
+for t in range(int(input())):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    print(f'#{t}', end=' ')
-    if solve(arr) > solve2(arr):
-        print(solve(arr))
+    print(f'#{t+1}', end=' ')
+    # print(arr)
+    if solve1() > solve2():
+        print(solve1())
     else:
-        print(solve2(arr))
+        print(solve2())
