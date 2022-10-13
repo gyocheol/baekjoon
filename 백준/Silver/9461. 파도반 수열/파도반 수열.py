1a4ab623d5d4f8a5b5f6018738823d6355693fdc
강교철 <96896754+gyocheol@users.noreply.github.com>
2022-10-13 17:24:45 +0900

    [Silver III] Title: 파도반 수열, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -2,17 +2,11 @@ import sys
 input = sys.stdin.readline
 
 
-def solve(x):
-    if x > 5:
-        DP = [0, 1, 1, 1, 2, 2] + [0] * (N-5)
-        for i in range(6, N+1):
-            DP[i] = DP[i-1] + DP[i-5]
-        return DP[x]
-    else:
-        DP = [0, 1, 1, 1, 2, 2]
-        return DP[x]
-
-
 for _ in range(int(input())):
     N = int(input())
-    print(solve(N))
+    DP = [0] * 101
+    DP[1] = DP[2] = DP[3] = 1
+    DP[4] = DP[5] = 2
+    for i in range(6, N+1):
+        DP[i] = DP[i-1] + DP[i-5]
+    print(DP[N])
