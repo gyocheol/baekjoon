5a4ce411218ddeb28ae91c29312351930ae1d72d
강교철 <96896754+gyocheol@users.noreply.github.com>
2022-10-24 14:16:53 +0900

    [Silver III] Title: 피보나치 함수, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,3 +1,34 @@
+'''
+# 시간초과..ㅜ
+from collections import deque
+import sys
+input = sys.stdin.readline
+# import time
+# starttime = time.time()
+
+def f(n):
+    while Q:
+        x = Q.popleft()
+        if x == 0:
+            ans[0] += 1
+        elif x == 1:
+            ans[1] += 1
+        if x-1 >= 1 or x-2 >= 1 or x-2 >= 0:
+            Q.append(x-1)
+            Q.append(x-2)
+
+N = int(input())
+for _ in range(N):
+    ans = [0, 0]
+    x = int(input())
+    Q = deque()
+    Q.append(x)
+    f(x)
+    print(*ans)
+
+# print(time.time()-starttime)
+'''
+
 dp = [[] for _ in range(41)]
 dp[0] = [1, 0]
 dp[1] = [0, 1]
