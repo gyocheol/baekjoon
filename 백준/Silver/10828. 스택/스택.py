ab7f91c8535617064d2a768ad10c0958f3138f0c
강교철 <96896754+gyocheol@users.noreply.github.com>
2022-12-04 19:47:29 +0900

    [Silver IV] Title: 스택, Time: 256 ms, Memory: 117664 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,27 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = deque()
+for _ in range(N):
+    order = input().split()
+    if order[0] == 'push':
+        stack.append(order[1])
+    elif order[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif order[0] == 'size':
+        print(len(stack))
+    elif order[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif order[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
