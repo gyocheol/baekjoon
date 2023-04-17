47c3a2908b20444b0afee1f926396af9577280e6
강교철 <96896754+gyocheol@users.noreply.github.com>
2023-04-18 00:44:38 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,20 +1,23 @@
 from collections import deque
 
-def bfs(x, y):
+
+def bfs(a, b):
     Q = deque()
-    Q.append((x, y))
+    Q.append((a, b))
+
     while Q:
         x, y = Q.popleft()
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
-            if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 1:
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == 1:
                 arr[nx][ny] = arr[x][y] + 1
                 Q.append((nx, ny))
 
-N, M = map(int, input().split())
-arr = [list(map(int, input())) for _ in range(N)]
-dx = [-1, 1, 0, 0]
-dy = [0, 0, 1, -1]
+
+n, m = map(int, input().split())
+arr = [list(map(int, input())) for _ in range(n)]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 bfs(0, 0)
-print(arr[N-1][M-1])
+print(arr[n-1][m-1])
