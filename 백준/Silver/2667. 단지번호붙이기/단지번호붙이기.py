6dcd2fcd101412267d8c7f00a29f9cd2d0ead95e
강교철 <96896754+gyocheol@users.noreply.github.com>
2023-04-20 00:26:52 +0900

    [Silver I] Title: 단지번호붙이기, Time: 68 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,41 +1,38 @@
 from collections import deque
 
+
 def bfs(x, y):
-    global total
     Q = deque()
     Q.append((x, y))
-    arr[x][y] = 2
-
+    visited[x][y] = True
+    total = 1
     while Q:
         x, y = Q.popleft()
-        for z in range(4):
-            nx = x + dx[z]
-            ny = y + dy[z]
-            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and  arr[nx][ny] == 1:
-                visited[nx][ny] = 2
-                total += 1
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] and not visited[nx][ny]:
                 Q.append((nx, ny))
+                visited[nx][ny] = True
+                total += 1
+    ans_list.append(total)
 
 
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
+n = int(input())
+arr = [list(map(int, input())) for _ in range(n)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
 
-N = int(input())
-arr = [list(map(int, input())) for _ in range(N)]
-visited = [[0]*N for _ in range(N)]
-
-all_cnt = 0
-cnt = []
-
-for i in range(N):
-    for j in range(N):
-        total = 1
+ans = 0
+ans_list = []
+visited = [[False] * (n+1) for _ in range(n+1)]
+for i in range(n):
+    for j in range(n):
         if arr[i][j] and not visited[i][j]:
             bfs(i, j)
-            cnt.append(total)
-            all_cnt += 1
+            ans += 1
 
-cnt.sort()
-print(all_cnt)
-for i in range(all_cnt):
-    print(cnt[i])
+print(ans)
+ans_list.sort()
+for i in ans_list:
+    print(i)
