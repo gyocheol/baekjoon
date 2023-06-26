3dc12a20b172664e1451b90ebd3b5f284cd5e2aa
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2023-06-26 22:28:05 +0900

    [Silver V] Title: 색종이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+ans_list = [[False] * 100 for _ in range(100)]
+
+for _ in range(t):
+    x, y = map(int, input().split())
+    for i in range(x-1, x+9):
+        for j in range(y-1, y+9):
+            ans_list[i][j] = True
+ans = 0
+for i in range(100):
+    ans += ans_list[i].count(True)
+print(ans)
