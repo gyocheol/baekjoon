17bb6fc36e16fed891a4c9d841ad4199f969a46d
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2023-06-29 00:14:04 +0900

    [Silver II] Title: 랜선 자르기, Time: 480 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,14 @@
+k, n = map(int, input().split())
+lan_list = [int(input()) for _ in range(k)]
+start, end = 1, max(lan_list)
+
+while start <= end:
+    mid = (start + end) // 2
+    lines = 0
+    for i in lan_list:
+        lines += i // mid
+    if lines >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
