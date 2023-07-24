7e28e30c05b7d00e1fcb75b9c5dd9cf08a51fe59
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2023-07-25 01:22:33 +0900

    [Silver IV] Title: solved.ac, Time: 152 ms, Memory: 36008 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.py
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -2,15 +2,12 @@ import sys
 input = sys.stdin.readline
 
 def r(num):
-    if num - int(num) >= 0.5:
-        return int(num) + 1
-    else:
-        return int(num)
-    
-    
+    return int(num + 0.5)
+
 n = int(input())
 if n != 0:
-    arr = sorted([int(input()) for _ in range(n)], key=lambda x:x)
+    arr = [int(input()) for _ in range(n)]
+    arr.sort()
     j_avg = r(n * 0.15)
     if j_avg != 0:
         print(r(sum(arr[j_avg:-j_avg]) / (n-2*j_avg)))
