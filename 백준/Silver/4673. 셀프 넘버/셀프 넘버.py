2d2d6076e97ba33f33a613a5aaa5af016fc93b39
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2023-08-27 23:57:06 +0900

    [Silver V] Title: 셀프 넘버, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,12 +1,12 @@
-def d():
-    l = []
-    for i in range(1,10000):
-        x = list(str(i))
-        for j in x:
-            i += int(j)
-        l.append(i)
+arr = [True] * 10001
+for i in range(1, 10000):
+    x = str(i)
+    total = 0
+    for j in x:
+        total += int(j)
+    if i + total < 10001:
+        arr[i+total] = False
 
-    for p in range(1,10000):
-        if p not in l:
-            print(p)
-d()
+for i in range(1, 10001):
+    if arr[i]:
+        print(i)
