d8d9124b2cce83f93742d508a86dcfd3f0f70e3c
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2023-09-21 03:24:36 +0900

    [Silver V] Title: 단어 나누기, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -1,14 +1,10 @@
 s = input()
 n = len(s)
-ans = []
+ans = "z" * n
 for i in range(n-2):
     for j in range(i+1, n-1):
         for z in range(j+1, n):
-            total = ""
-            total += s[:j][::-1]
-            total += s[j:z][::-1]
-            total += s[z:][::-1]
-            if total not in ans:
-                ans.append(total)
-ans.sort()
-print(ans[0])
+            total = s[:j][::-1]+s[j:z][::-1]+s[z:][::-1]
+            if total < ans:
+                ans = total
+print(ans)
