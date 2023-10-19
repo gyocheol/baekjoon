f580ac6f5f1fb9207eabe1e20162aedbef74a5e8
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2023-10-20 04:01:02 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
@@ -1,14 +1,14 @@
 seed = int(input())
 stocks = [0] + list(map(int, input().split()))
 j, s = seed, seed
-s_stock, j_stock = [0] * 15, [0] * 15
+s_stock, j_stock = 0, 0
 increase, decrease = 0, 0
 for i in range(1, 15):
-    j_buy = j // stocks[i]
-    if j_buy:
-        j_stock[i] = j_buy
-        j = j - j_buy * stocks[i]
-
+    # 준현 매수
+    if j:
+        j_stock += j // stocks[i]
+        j = j % stocks[i]
+    # 성민
     if stocks[i-1] < stocks[i]:
         increase += 1
         decrease = 0
@@ -17,18 +17,18 @@ for i in range(1, 15):
         increase = 0
     else:
         increase, decrease = 0, 0
-    s_buy = s // stocks[i]
-    if increase >= 3:
-        for k in range(i):
-            if s_stock[k]:
-                s += s_stock[k] * stocks[i]
-                s_stock[k] = 0
+    # 매도
+    if increase == 3:
+        if s_stock:
+            s += s_stock * stocks[i]
+            s_stock = 0
+    # 매수
     elif decrease >= 3:
-        s_stock[i] = s_buy
-        s -= s_buy * stocks[i]
-for i in range(15):
-    j += j_stock[i] * stocks[-1]
-    s += s_stock[i] * stocks[-1]
+        s_stock += s // stocks[i]
+        s = s % stocks[i]
+# 매도
+j += j_stock * stocks[-1]
+s += s_stock * stocks[-1]
 
 if j > s:
     print("BNP")
