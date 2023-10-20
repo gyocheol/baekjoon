e55144dbe3e05700d76578474c0e4e0fd1127ba7
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2023-10-20 19:20:31 +0900

    [Silver V] Title: 진짜 메시지, Time: 276 ms, Memory: 111512 KB -BaekjoonHub

--- 백준/Silver/9324. 진짜 메시지/진짜 메시지.py
+++ 백준/Silver/9324. 진짜 메시지/진짜 메시지.py
@@ -1,14 +1,20 @@
+import sys
+input = sys.stdin.readline
+
 for _ in range(int(input())):
     d = {}
     flag = False
     word, ans = input(), "OK"
     for i in range(len(word)):
+        # word[i+1]로 체크 한 것을 넘어가기 위해
         if flag:
             flag = False
             continue
+        # 딕셔너리에 추가
         try:d[word[i]] += 1
         except:d[word[i]] = 1
-
+        
+        # 3번째 중복된 알파벳이 왔을 때
         if d[word[i]] == 3:
             if i == len(word)-1 or word[i] != word[i+1]:
                 ans = "FAKE"
