910a99651e7a293be0bc39b410c04cc0587d0ef6
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2024-01-02 15:43:02 +0900

    [Silver V] Title: 연도 진행바, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1340. 연도 진행바/연도 진행바.py
+++ 백준/Silver/1340. 연도 진행바/연도 진행바.py
@@ -1,39 +1,14 @@
-months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-month, day, year, time = input().split(" ")
-day = day[:-1]
-time, minute = time.split(":")
-flag = False
-total = 0
-now = 0
-m = 24*60 # 일을 분으로 변경
+# 다시 풀어보기
+month, d, y, t = input().split(" ")
+d = int(d[:-1])
+y = int(y)
+h, m = map(int, t.split(":"))
+months_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+months_day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+# 윤년일 때 2월에 +1
+if not y % 400 or not y % 4 and y % 100:
+    months_day[1] += 1
+total = sum(months_day) * 24 * 60
+now = (sum(months_day[:months_name.index(month)]) + d-1) * 24 * 60 + h * 60 + m
 
-# 윤년 계산
-year = int(year)
-if not year % 400 or not year % 4 and year % 100:
-    total = 366*m
-    flag = True
-else:
-    total = 365*m
-
-# 달 계산
-now_month = months.index(month)
-to = [1,3,5,7,8,10,12] # 31일
-for i in range(now_month):
-    if i+1 == 2:
-        if flag:
-            now += 29*m
-        else:
-            now += 28*m
-    elif i+1 in to:
-        now += 31*m
-    else:
-        now += 30*m
-
-# 일 계산
-now += (int(day)-1)*m
-
-# 시,분 계산
-now += int(time) * 60 + int(minute)
-
-# 결과
 print(now/total*100)
