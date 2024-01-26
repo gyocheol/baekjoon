a40d3621da4b303f9efb2b436a52119ef7a92903
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2024-01-26 18:01:21 +0900

    [Silver V] Title: 욱제는 결정장애야!!, Time: 176 ms, Memory: 139288 KB -BaekjoonHub

--- 백준/Silver/14646. 욱제는 결정장애야！！/욱제는 결정장애야！！.py
+++ 백준/Silver/14646. 욱제는 결정장애야！！/욱제는 결정장애야！！.py
@@ -1,6 +1,6 @@
 import sys
 input = sys.stdin.readline
-
+''' 느림
 n = int(input())
 nums = list(map(int, input().split()))
 check_list = []
@@ -11,6 +11,22 @@ for num in nums:
         check_list.append(num)
         total += 1
     else:
+        total -= 1
+    ans = max(ans, total)
+print(ans)
+'''
+
+n = int(input())
+nums = list(input().split())
+check = set()
+total = 0
+ans = 0
+for num in nums:
+    if num not in check:
+        check.add(num)
+        total += 1
+    else:
+        check.remove(num)
         total -= 1
     ans = max(ans, total)
 print(ans)
