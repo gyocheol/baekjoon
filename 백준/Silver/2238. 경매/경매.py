deeedba9ce911cab0e416611c40ea3365b076058
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2024-06-05 16:09:06 +0900

    [Silver V] Title: 경매, Time: 2832 ms, Memory: 39308 KB -BaekjoonHub

---
+++ 백준/Silver/2238. 경매/경매.py
@@ -0,0 +1,17 @@
+u, n = map(int, input().split())
+prices = [0] * 10001
+names = [[] for _ in range(10001)]
+
+for i in range(n):
+    name, s_price = input().split()
+    price = int(s_price)
+    prices[price] += 1
+    names[price].append(name)
+
+min_val = 10001
+min_idx = 10001
+for i in range(10001):
+    if prices[i] and min_val > prices[i]:
+        min_val = prices[i]
+        min_idx = i
+print(names[min_idx][0], min_idx)
