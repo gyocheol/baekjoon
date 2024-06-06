13f8eb214377714ac79ffd671f34a415d67b836c
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2024-06-06 19:17:52 +0900

    [Silver V] Title: Olympiad Pizza, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15235. Olympiad Pizza/Olympiad Pizza.py
+++ 백준/Silver/15235. Olympiad Pizza/Olympiad Pizza.py
@@ -1,4 +1,7 @@
+'''
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 n = int(input())
 participants = list(map(int, input().split()))
@@ -14,3 +17,17 @@ while Q:
         Q.append(q)
 
 print(*time)
+''' # 위는 시간이 오래 걸림 아래는 빼는 방식
+import sys
+input = sys.stdin.readline
+n = int(input())
+participants = list(map(int, input().split()))
+ans = [0] * n
+time = 0
+while sum(participants) > 0:
+    for i in range(n):
+        if participants[i] > 0:
+            participants[i] -= 1
+            time += 1
+            ans[i] = time
+print(*ans)
