e2279aa06f3f630877317dd7272dcc9b2969e62e
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2024-06-14 18:54:32 +0900

    [Silver V] Title: (중략), Time: 40 ms, Memory: 31552 KB -BaekjoonHub

---
+++ 백준/Silver/21966. （중략）/（중략）.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+S = input()
+if n <= 25:
+    print(S)
+else:
+    if len(S[11:-12].split(".")) < 2:
+        print(S[:11]+"..."+S[-12:])
+    elif not S[11:-12].split(".")[1]:
+        print(S[:11]+"..."+S[-12:])
+    else:
+        print(S[:9]+"......"+S[-11:])
