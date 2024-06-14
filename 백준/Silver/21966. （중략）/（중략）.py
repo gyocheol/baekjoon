3ed66f958d6fcb3209375606c0ac7c19ad329732
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2024-06-14 18:59:05 +0900

    [Silver V] Title: (중략), Time: 40 ms, Memory: 31552 KB -BaekjoonHub

--- 백준/Silver/21966. （중략）/（중략）.py
+++ 백준/Silver/21966. （중략）/（중략）.py
@@ -6,9 +6,7 @@ S = input()
 if n <= 25:
     print(S)
 else:
-    if len(S[11:-12].split(".")) < 2:
-        print(S[:11]+"..."+S[-12:])
-    elif not S[11:-12].split(".")[1]:
+    if len(S[11:-12].split(".")) < 2 or not S[11:-12].split(".")[1]:
         print(S[:11]+"..."+S[-12:])
     else:
         print(S[:9]+"......"+S[-11:])
