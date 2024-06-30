62283402c9cc84a3f9ed48fa395c78c7cedd5557
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2024-06-30 18:48:26 +0900

    [Silver V] Title: 개미 수열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/28292. 개미 수열/개미 수열.py
+++ 백준/Silver/28292. 개미 수열/개미 수열.py
@@ -1,8 +1,7 @@
+# 애드훅
 n = int(input())
-
-arr=[1, 1, 2, 2, 2, 3]
-
-if n >= 6 :
-    print(arr[-1])
-else :
-    print(arr[n-1])
+ans = [0, 1, 1, 2, 2, 2]
+if n >= 6:
+    print(3)
+else:
+    print(ans[n])
