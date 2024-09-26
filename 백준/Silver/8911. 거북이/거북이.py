2326f70829a607db730930002c0c5ade100d72cf
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2024-09-26 18:02:04 +0900

    [Silver III] Title: 거북이, Time: 784 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/8911. 거북이/거북이.py
+++ 백준/Silver/8911. 거북이/거북이.py
@@ -6,7 +6,8 @@ dx = [1, 0, -1, 0]  # 시계방향
 dy = [0, 1, 0, -1]
 for _ in range(n):
     order = input().rstrip()
-    min_x, max_x, min_y, max_y = 0, 0, 0, 0
+    total_x = [0]
+    total_y = [0]
     idx = 0         # 시작은 북
     x, y = 0, 0
     for i in order:
@@ -14,15 +15,16 @@ for _ in range(n):
         if i == 'F':
             x += dx[idx]
             y += dy[idx]
+            total_x.append(x)
+            total_y.append(y)
         elif i == 'B':
             x -= dx[idx]
             y -= dy[idx]
+            total_x.append(x)
+            total_y.append(y)
         elif i == 'L':
             idx -= 1
         elif i == 'R':
             idx += 1
-        min_x = min(x, min_x)
-        min_y = min(y, min_y)
-        max_x = max(x, max_x)
-        max_y = max(y, max_y)
-    print(abs(max_x-min_x)*abs(max_y-min_y))
+
+    print(abs(max(total_x)-min(total_x))*abs(max(total_y)-min(total_y)))
