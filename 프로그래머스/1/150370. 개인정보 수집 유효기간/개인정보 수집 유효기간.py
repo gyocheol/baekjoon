f686d258a273f50368241c84b11b9fbd6bd8181a
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2024-10-01 19:27:44 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.15 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,26 @@
+def solution(today, terms, privacies):
+    ans = []
+    t = {terms[i].split()[0]:int(terms[i].split()[1]) for i in range(len(terms))}
+    y, m, d = today.split(".")
+    # 모든 기간에 대한 날짜를 구하기
+    total = {}
+    for k, v in t.items():
+        yy, mm = int(y), int(m)
+        if 12 < v:
+            yy -= v//12
+            mm -= v%12
+            if mm <= 0:
+                yy -= 1
+                mm += 12
+        elif mm <= v:
+            yy -= 1
+            mm += 12 - v
+        else:
+            mm -= v
+        total[k] = f"{yy}.{mm:02}.{d}"
+    print(total)
+    for i in range(len(privacies)):
+        date, term = privacies[i].split()
+        if total[term] >= date:
+            ans.append(i+1)
+    return ans
