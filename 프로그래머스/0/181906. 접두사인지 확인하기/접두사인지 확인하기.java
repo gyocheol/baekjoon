075e1c7d0dce44e0dc67fa9ebfa8dfec0bea741e
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2025-09-24 14:50:48 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.04 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,16 +1,18 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        int ans = 0;
-        if (my_string.length() < is_prefix.length()){
-            return ans;
-        }
-        for (int i = 1; i < my_string.length(); i++) {
-            String str = my_string.substring(0, i);
-            if (str.equals(is_prefix)) {
-                return 1;
-            }
-        }
+//         int ans = 0;
+//         if (my_string.length() < is_prefix.length()){
+//             return ans;
+//         }
+//         for (int i = 1; i < my_string.length(); i++) {
+//             String str = my_string.substring(0, i);
+//             if (str.equals(is_prefix)) {
+//                 return 1;
+//             }
+//         }
         
-        return ans;
+//         return ans;
+        if (my_string.startsWith(is_prefix)) return 1;
+        return 0;
     }
 }
