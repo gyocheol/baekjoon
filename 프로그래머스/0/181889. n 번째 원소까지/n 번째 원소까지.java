530b04e01116e5d86445825e0a329ddfb21ec43a
gyocheol Kang <96896754+gyocheol@users.noreply.github.com>
2025-10-02 14:47:37 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -2,14 +2,14 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        // Arrays.copyOfRange()
-        return Arrays.copyOfRange(num_list, 0, n);
+        // // Arrays.copyOfRange()
+        // return Arrays.copyOfRange(num_list, 0, n);
         
-        // // for문
-        // int[] ans = new int[n];
-        // for (int i = 0; i < n; i++) {
-        //     ans[i] = num_list[i];
-        // }
-        // return ans;
+        // for문
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = num_list[i];
+        }
+        return ans;
     }
 }
